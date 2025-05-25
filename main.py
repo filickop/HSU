96cdd961546f3b35ef7ed45a04f71c1b2b86154b
Michal <michal.zifcak12@gmail.com>
2025-05-25 21:29:22 +0200

    Implement Super Point with Super Glue and refactor data generation

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ datasetGenerator = DatasetGenerator(
     'dataset/Note/keypoints/note_keypoints.json')
 
 datasetGenerator.generate(100)
-matcher = SuperPointSuperGlueMatcher(
-    'dataset/Note/keypoints/note_keypoints.json',
-    'dataset/Note/note_000.png')
 
-img2, mkpt0, mkpt1 = matcher.match(2)
-mkpts0_0, mkpts0_1 = matcher.select_nearest_keypoint(mkpt0, mkpt1, (90, 70))
-matcher.visualize(img2, mkpts0_0, mkpts0_1)
+developer = ""
+if developer == "MZ":
+    matcher = SuperPointSuperGlueMatcher(
+        'dataset/Note/keypoints/note_keypoints.json',
+        'dataset/Note/note_000.png')
+
+    img2, mkpt0, mkpt1 = matcher.match(2)
+    mkpts0_0, mkpts0_1 = matcher.select_nearest_keypoint(mkpt0, mkpt1, (90, 70))
+    matcher.visualize(img2, mkpts0_0, mkpts0_1)
