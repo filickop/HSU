09ea0aec900846c7fdbb92bf4a97fb8e6d3008e7
patri <patrikfilicko@gmail.com>
2025-05-26 21:34:48 +0200

    oprava

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 from torch.utils.data import DataLoader
 
 from dataset.Note.NotePairKeypointDataset import NotePairKeypointDataset
-from models.SuperPointSuperGlue.SuperPointSuperGlueMatcher import SuperPointSuperGlueMatcher
 from models.LoFTR.LoFTR import LoFTRRunner
+from models.SuperPointSuperGlue.SuperPointSuperGlueMatcher import SuperPointSuperGlueMatcher
 from dataset.DatasetGenerator import DatasetGenerator
 
 datasetGenerator = DatasetGenerator(
-    'dataset/Note/note_000.png',
-    'dataset/Note/note_000.json',
-    'dataset/Note/images',
-    'dataset/Note/keypoints/note_keypoints.json')
+    [
+        'dataset/Note/note_000.png',
+        'dataset/Lepidla/lepidla1/lepidla1.jpg',
+        'dataset/Lepidla/lepidla2/lepidla2.jpg',
+        'dataset/Lepidla/lepidla3/lepidla3.jpg',
+        'dataset/Lepidla/lepidla4/lepidla4.jpg',
+        'dataset/Lepidla/lepidla5/lepidla5.jpg'
+    ],
+    [
+        'dataset/Note/note_000.json',
+        'dataset/Lepidla/lepidla1/lepidla1.json',
+        'dataset/Lepidla/lepidla2/lepidla2.json',
+        'dataset/Lepidla/lepidla3/lepidla3.json',
+        'dataset/Lepidla/lepidla4/lepidla4.json',
+        'dataset/Lepidla/lepidla5/lepidla5.json'
+    ],
+    [
+        'dataset/Note/images',
+        'dataset/Lepidla/lepidla1/images',
+        'dataset/Lepidla/lepidla2/images',
+        'dataset/Lepidla/lepidla3/images',
+        'dataset/Lepidla/lepidla4/images',
+        'dataset/Lepidla/lepidla5/images'
+    ],
+    [
+        'dataset/Note/keypoints/note_keypoints.json',
+        'dataset/Lepidla/lepidla1/keypoints/lepidla1.json',
+        'dataset/Lepidla/lepidla2/keypoints/lepidla2.json',
+        'dataset/Lepidla/lepidla3/keypoints/lepidla3.json',
+        'dataset/Lepidla/lepidla4/keypoints/lepidla4.json',
+        'dataset/Lepidla/lepidla5/keypoints/lepidla5.json'
+    ])
 
-#datasetGenerator.generate(100)
+datasetGenerator.generate(100)
 
-developer = "PF"
+developer = "MZ"
 if developer == "MZ":
     matcher = SuperPointSuperGlueMatcher(
-        'dataset/Note/keypoints/note_keypoints.json',
-        'dataset/Note/note_000.png')
+        'dataset/Lepidla/lepidla1/lepidla1.json',
+        'dataset/Lepidla/lepidla1/lepidla1.jpg',)
 
-    img2, mkpt0, mkpt1 = matcher.match(2)
-    mkpts0_0, mkpts0_1 = matcher.select_nearest_keypoint(mkpt0, mkpt1, (90, 70))
+    img2, mkpt0, mkpt1 = matcher.match(4)
+    mkpts0_0, mkpts0_1 = matcher.select_nearest_keypoint(mkpt0, mkpt1, (600, 595))
     matcher.visualize(img2, mkpts0_0, mkpts0_1)
 
 elif developer == "PF":
